bec2381f370e2d980ac4beeb97180cf99b87a300
Cem Tosun <134544650+Kilopi3142@users.noreply.github.com>
2023-06-19 12:02:49 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+	   <h1>Kilopi LOP token basic info</h1>
+	   <br/>
+	   <h3>OLD (Obsolete) Token Address (Harmony): 0x1791c97603b4695f53A4f1c02ca0efB74C44b310</h3>
+	   <h3>Harmony Explorer Link: 
+	   <a href="https://explorer.harmony.one/address/0x1791c97603b4695f53a4f1c02ca0efb74c44b310" target="_blank"
+                rel="noreferrer"> https://explorer.harmony.one/address/0x1791c97603b4695f53a4f1c02ca0efb74c44b310 </a>
+				
+				</h3>
+	   <br/>
+	   <br/>
+	   <h3>Official Token Address (Harmony): 0x09e6E20FF399c2134C14232E172ce8ba2b03017E</h3>
+	   <h3>Harmony Explorer Link: 
+	   <a href="https://explorer.harmony.one/address/0x09e6e20ff399c2134c14232e172ce8ba2b03017e" target="_blank"
+                rel="noreferrer"> https://explorer.harmony.one/address/0x09e6e20ff399c2134c14232e172ce8ba2b03017e </a>
+				
+				</h3>
+	   <br/>
+	   <br/>
+	   <h3>Official Token Address (Binance): 0x20B4620a767D6dCcBB9314104D5Cf0d08d1F7045</h3>
+	   <h3>Binance Explorer Link: <a href="https://bscscan.com/token/0x20b4620a767d6dccbb9314104d5cf0d08d1f7045" target="_blank"
+                rel="noreferrer"> https://bscscan.com/token/0x20b4620a767d6dccbb9314104d5cf0d08d1f7045</a>
+				</h3>
+	   <br/>
+	   <br/>
+	   <h3>Name: Kilopi Symbol: LOP Decimals: 6</h3>
+	   <h3>Total Supply: 5.000.000.000,00 ( 5 Billion ) ( No Mint Function )</h3>
+	   
+	  
+	  
+    </div>
+  );
+}
+
+export default App;
